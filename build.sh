71413684e30d174cebe7f192fac501e80ff470f6
Oyegbule <oyegbuleeniola04@gmail.com>
2026-03-16 13:07:21 +0100

    added build script to fix database error

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Python packages
+pip install -r requirements.txt
+
+# Build Tailwind CSS
+npm install
+npx tailwindcss -i ./static/src/style.css -o ./static/dist/style.css --minify
+
+# Collect static files and run database migrations
+python manage.py collectstatic --no-input
+python manage.py migrate
