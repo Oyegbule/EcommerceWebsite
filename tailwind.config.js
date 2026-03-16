872bf6dfdebe5072ce33fd77a1004919d45b9bfc
Oyegbule <oyegbuleeniola04@gmail.com>
2026-03-16 12:32:37 +0100

    Initial commit with Tailwind and Django

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",        // This scans EVERY html file in your project
+    "./**/static/**/*.js", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
