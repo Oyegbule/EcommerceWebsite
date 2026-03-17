c9ce15f6b0b970db4fadc73892abc8aa29eb3deb
Oyegbule <oyegbuleeniola04@gmail.com>
2026-03-17 13:07:00 +0100

    Force uploading mediafiles to Render

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # Install Python packages
 pip install -r requirements.txt
 
+mkdir -p mediafiles
+
 # Build Tailwind CSS
 npm install
 npx tailwindcss -i ./static/src/style.css -o ./static/dist/style.css --minify
