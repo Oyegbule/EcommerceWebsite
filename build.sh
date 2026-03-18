beeb5c694d59ac884ff01f54dba27cacd8fbbb6b
Oyegbule <oyegbuleeniola04@gmail.com>
2026-03-18 15:54:26 +0100

    added admin auto-create

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Install Python packages
 pip install -r requirements.txt
 
-mkdir -p mediafiles
+
 
 # Build Tailwind CSS
 npm install
@@ -14,3 +14,21 @@ npx tailwindcss -i ./static/src/style.css -o ./static/dist/style.css --minify
 # Collect static files and run database migrations
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# EDIT THESE TWO LINES:
+username = 'Esther'
+password = 'Esther12'
+email = 'admin@example.com'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print(f"Superuser '{username}' created successfully!")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
